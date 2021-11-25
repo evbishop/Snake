2473f57afab0b186c2770146ac7f860e2e295c29
andden <andden96@gmail.com>
2021-11-25 15:25:57 +0600

    Моя попытка спавна и синхронизации еды

--- Assets/Scripts/Food/FoodSpawner.cs
+++ Assets/Scripts/Food/FoodSpawner.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class FoodSpawner : MonoBehaviour
+public class FoodSpawner : NetworkBehaviour
 {
     [SerializeField] GameObject foodPrefab;
     [SerializeField] float xSize = 8f, zSize = 8f;
 
-    void Start()
+    public override void OnStartServer()
     {
         SpawnFood();
     }
 
+    [Server]
     public void SpawnFood()
     {
         Vector3 pos = new Vector3(
             Random.Range(-xSize, xSize),
             foodPrefab.transform.position.y,
             Random.Range(-zSize, zSize));
-        Instantiate(foodPrefab, pos, foodPrefab.transform.rotation);
+        var foodInstance = Instantiate(foodPrefab, pos, foodPrefab.transform.rotation);
+        NetworkServer.Spawn(foodInstance);
     }
 }
