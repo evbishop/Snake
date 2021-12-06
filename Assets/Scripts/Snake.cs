0215900f96580238b141be41add3a41924360252
andden <andden96@gmail.com>
2021-12-06 15:52:31 +0600

    Сделали увеличение скорости змеи после принятия пищи

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Mirror;
+using System;
 
-public class Snake : MonoBehaviour
+public class Snake : NetworkBehaviour
 {
-    [SerializeField] float speed = 3f, rotationSpeed = 180f, speedChange = 0.5f;
+    [SerializeField] float rotationSpeed = 180f, speedChange = 0.5f;
     [SerializeField] GameObject tailPrefab;
 
-    public float Speed { get { return speed; } }
+    [SerializeField] [SyncVar]
+    float speed = 3f;
+    public float Speed 
+    {
+        get { return speed; }
+        private set { speed = value; }
+    }
+
+    public override void OnStartServer()
+    {
+        Food.ServerOnFoodEaten += ServerHandleFoodEaten;
+    }
+
+    public override void OnStopServer()
+    {
+        Food.ServerOnFoodEaten -= ServerHandleFoodEaten;
+    }
+
+    void ServerHandleFoodEaten(GameObject playerWhoAte)
+    {
+        if (gameObject == playerWhoAte)
+            Speed += speedChange;
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
     }
 
