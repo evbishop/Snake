87595bb83629982b3ee79a2d03eb365922fd7ad2
evbishop <anddenissyuk@gmail.com>
2021-12-13 18:20:35 +0600

    Сделали обработку смерти змеек

--- Assets/Scripts/Player/PlayerSnake.cs
+++ Assets/Scripts/Player/PlayerSnake.cs
@@ -6,5 +6,28 @@ using System;
 
 public class PlayerSnake : NetworkBehaviour
 {
-    
+    [SerializeField] TailSpawner tailSpawner;
+
+    [ServerCallback]
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out NetworkIdentity networkIdentity)
+            && networkIdentity.connectionToClient == connectionToClient)
+            return;
+        switch (other.tag)
+        {
+            case "Border":
+            case "Player":
+            case "Tail":
+                DestroySelf();
+                break;
+        }
+    }
+
+    void DestroySelf()
+    {
+        foreach (var tail in tailSpawner.Tails)
+            NetworkServer.Destroy(tail);
+        NetworkServer.Destroy(gameObject);
+    }
 }
