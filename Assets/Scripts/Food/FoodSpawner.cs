b368c71447afbe07a249e38dbaca0ac54d15dd6b
evbishop <anddenissyuk@gmail.com>
2022-01-13 19:29:01 +0600

    Revert "Моя попытка спавна и синхронизации еды"
    This reverts commit 2473f57afab0b186c2770146ac7f860e2e295c29.

--- Assets/Scripts/Food/FoodSpawner.cs
+++ Assets/Scripts/Food/FoodSpawner.cs
@@ -1,26 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Mirror;
 using UnityEngine;
 
-public class FoodSpawner : NetworkBehaviour
+public class FoodSpawner : MonoBehaviour
 {
     [SerializeField] GameObject foodPrefab;
     [SerializeField] float xSize = 8f, zSize = 8f;
 
-    public override void OnStartServer()
+    void Start()
     {
         SpawnFood();
     }
 
-    [Server]
     public void SpawnFood()
     {
         Vector3 pos = new Vector3(
             Random.Range(-xSize, xSize),
             foodPrefab.transform.position.y,
             Random.Range(-zSize, zSize));
-        var foodInstance = Instantiate(foodPrefab, pos, foodPrefab.transform.rotation);
-        NetworkServer.Spawn(foodInstance);
+        Instantiate(foodPrefab, pos, foodPrefab.transform.rotation);
     }
 }
